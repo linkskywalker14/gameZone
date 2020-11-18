aa20d1e29f10da4fec30c12f73fd8894bc42f61c
Nick LS Whelan <Nicholas.LS.Whelan@gmail.com>
2020-11-18 14:50:23 -0800

    Significant reorganization. Mostly in the JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,121 @@
-//Global Variables. Have none by the end. 
+/* DEAL WITH THIS LATER
+
+function Round(player, rolls, ending, total) {
+  this.player = player;
+  this.rolls = rolls;
+  this.ending = ending;
+  this.total = total;
+}
+
+rounds.push(new Round());
+
+*/
+
+//PIG DICE
+//1. Global Variables
+//2. Ancilary Modules
+//3. Active Modules
+//4. User Interface Logic
+
+//1. Global Variables
+//Unfortunately, after thorough googling, the only way I can find to pass variables through an onClick event is to use React.
+//Since this would seem necessary, and since I haven't learned React, I believe global variables are necessary.
 
 let playerScore = 0;
 let robotScore = 0;
 let roundTotal = 0;
+
+//The three above seem necessary, given my current limitations. The three below haven't been assessed yet.
+
 let cheating = false;
+let rounds = [];
 player = true;
 
+//######
+//2. Ancilary Modules
+// 1, 2, 3--an Index
+//######
 
+//The displayFace function arranges the pips on the die. 
+function displayFace(face){
+  const pip = '<img src="img/pip.gif" />';
+  const nopip = '<img src="img/nopip.gif" />';
+  switch (face){
+    case (1): 
+      $("#theDie").html(nopip + nopip + nopip + "<br />" + nopip + pip + nopip + "<br />" + nopip + nopip + nopip);
+      break;
+    case (2): 
+      $("#theDie").html(nopip + nopip + pip + "<br />" + nopip + nopip + nopip + "<br />" + pip + nopip + nopip);
+      break;
+    case (3):  
+      $("#theDie").html(nopip + nopip + pip + "<br />" + nopip + pip + nopip + "<br />" + pip + nopip + nopip);
+      break;
+    case (4):  
+      $("#theDie").html(pip + nopip + pip + "<br />" + nopip + nopip + nopip + "<br />" + pip + nopip + pip);
+      break;
+    case (5):  
+      $("#theDie").html(pip + nopip + pip + "<br />" + nopip + pip + nopip + "<br />" + pip + nopip + pip);
+      break;
+    case (6):  
+      $("#theDie").html(pip + nopip + pip + "<br />" + pip + nopip + pip + "<br />" + pip + nopip + pip);
+      break;
+    default: $("#theDie").html("What did you do!?");
+  }
+}
 
+//the rollD6 function returns a random number between 1 and 6. If cheating is active, the number is weighted.
 function rollD6(){
   if (cheating === true){
     const loadedD6 = [1, 2, 3, 4, 4, 5, 6, 6];
     const x = parseInt(Math.random() * 8 + 0);
-    console.log("I'm cheating by rolling a d8. I got a " + x);
     return loadedD6[x];
   } else {
     return parseInt(Math.random() * 5 + 1);
   }
 }
 
+//the winCheck function is run after every die roll to see if the active player has gotten enough points to win.
 function winCheck(){
   if (player && playerScore + roundTotal >= 100){
     $("#controls").hide();
     $("#endgame").show();
     $("#victor").append("WE ARE SAVED! THE PLAYER WINS!");
+    $("#pregame").show();
   } else if (!player && robotScore + roundTotal >= 100){
     $("#controls").hide();
     $("#endgame").show();
     $("#victor").append("WE ARE DOOMED! THE EVIL ROBO-PIG HAS WON!");
+    $("#pregame").show();
   }
 }
 
-function click(){
-  const face = rollD6();
-  if (face === 1) {
-    if (player === true){
-      console.log("Boo, you have rolled a 1, player! The " + roundTotal + " points you earned this round must be discarded, and now the turn passes to the EVIL COMPUTER!");
+//the roundEnd function is INCOMPLETE. It runs either when a 1 is rolled, or when hold occurs. 
+function roundEnd(hold){
+  player = !player;
+  if (hold === true){
+    if(player === false){
+      playerScore = playerScore + roundTotal;
+      roundTotal = 0;
+      console.log("Player chooses to end with " + playerScore + " points. Turn passes to computer.");
+      aiPlayer();
+    } else {
+      robotScore = robotScore + roundTotal;
+      roundTotal = 0;
+      console.log("Computer chooses to end with " + robotScore + " points. Turn passes to player.");
+    }
+  } else {
+    if (player === false){
+      console.log("!!!THE PLAYER ROLLED A ONE!!!");
       roundTotal = 0;
       aiPlayer();
     } else {
-      console.log("Huzzah! The EVIL COMPUTER has rolled a 1! Now we can discard the " + roundTotal + " points it accumulated during this round!");
+      console.log("!!!THE COMPUTER ROLLED A ONE!!!");
       roundTotal = 0;
-    }
-  } else {
-    roundTotal = roundTotal + face;
-    winCheck();
-
-    switch (player) {
-      case (true):
-        console.log("Congratulations, player! You have rolled a " + face + " and now have a running total for this round of " + roundTotal);
-        break;
-      default:
-        console.log("Oh dear! The EVIL COMPUTER has rolled a " + face + " and now has a running total of " + roundTotal + " for this round!");
     }
   }  
 }
 
-function hold(){
-  playerScore = playerScore + roundTotal;
-  roundTotal = 0;
-  console.log("The player has chosen to end their turn with " + playerScore + " points. The turn now passes to the EVIL COMPUTER.");
-  aiPlayer();
-}
-
+//the aiGoal function runs at the beginning of the AI turn. It decides what number the AI will try to reach before choosing to end its turn.
 function aiGoal(){
   if (100 <= robotScore + 20) {
     console.log("The EVIL COMPUTER speaks: 'Looks like we're in end game now, HOOMAN!'");
@@ -76,36 +129,86 @@ function aiGoal(){
   }
 }
 
-function aiPlayer(){
-  player = false;
-  goal = aiGoal();
+//######
+//3. Active Modules
+// 1, 2, 3--an Index
+//######
 
+//The initialize function runs when the player wishes to start a new game.
+function initialize(){
+  $("#pregame").hide();
+  $("#controls").show();
+  playerScore = 0;
+  robotScore = 0;
+  roundTotal = 0;
+}
+
+//The click function runs whenever either player rolls the dice.
+function click(){
+  const face = rollD6();
+  displayFace(face);
+  if (face === 1) {
+    roundEnd();
+  } else {
+    roundTotal = roundTotal + face;
+    winCheck();
+    switch (player) {
+      case (true):
+        console.log("players rolls " + face + ". Round total is now " + roundTotal);
+        break;
+      default:
+        console.log("EVIL PIG ROLLS " + face + ". EVIL TOTAL IS NOW  " + roundTotal);
+    }
+  } 
+}
+
+//the hold function runs when the player chooses to end their turn.
+function hold(){
+  const hold = true;
+  roundEnd(hold);
+}
+
+//the aiPlayer function runs anytime it stops being the player's turn.
+function aiPlayer(){
+  const goal = aiGoal();
   do {
     click();
-    if (roundTotal === 0) {
-      player = true;
+    if (roundTotal === 0){
       break;
     }
   } while (robotScore + roundTotal < goal);
-
-  robotScore = robotScore + roundTotal;
-  roundTotal = 0;
-  console.log("COMPUTER ends the round with " + robotScore + " points.");
-  player = true;
+  if (roundTotal != 0){
+    hold();
+  }
 }
 
+//the cheatMode function runs when the player clicks the cheating button.
+function cheatMode(){
+  cheating = !cheating;
+  if (cheating === true){
+    $("#theDie").css({'background-color':'#FFA340'});
+  } else {
+    $("#theDie").css({'background-color':'#FAFADA'});
+  }
+}
 
-
-
-// USER INTERFACE LOGIC
+//######
+//4. User Interface Logic
+//######
 
 $(document).ready(function() {
 
+  $("#pregame").on("click", ".start",function() {
+    initialize();
+  });  
   $("#controls").on("click", ".roll",function() {
     click();
   });
   $("#controls").on("click", ".hold",function() {
     hold();
+  });
+  $("#controls").on("click", ".cheat",function() {
+    cheatMode();
   });
 
 });
