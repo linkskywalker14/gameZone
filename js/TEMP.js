be0f525830a8f2f9754527dada7cf48cd91fd7a8
Nick LS Whelan <Nicholas.LS.Whelan@gmail.com>
2021-02-01 16:17:41 -0800

    lvl 2 ai complete.

--- js/TEMP.js
+++ js/TEMP.js
@@ -1,22 +1,32 @@
+//TIC TAC TOE
+//1. Global Variables, Constructors, Prototypes
+//2. Functions (Back End)
+//3. Functions (Player Controls)
+//4. Functions (Options Menu)
+//5. User Interface Logic
+
+//######
+//1. Global Variables, Constructors, Prototypes
+//######
+
+//Eventually all of these will need to be Objects
+
 function Board(toprow, midrow, botrow, won, ai, dude) {
   this.toprow = toprow;
   this.midrow = midrow;
   this.botrow = botrow;
   this.won = won;
-  this.ai = ai;  //0 is human v human, 1 is random, 2 has some strategy?
+  this.ai = ai;  //0 is human v human, 1 is random, 2 blocks win attempts, future: 3 pursues wins using the same functions used to ID blocks.
   this.dude = dude;
 }
 
 let game1 = new Board ([],[],[], false, 0, "X");
 
-function newRound(ai) {
-  $("#playAgain").hide();
-  game1 = new Board ([],[],[], false, ai, "X");
-  $("#NW, #NC, #NE, #CW, #CC, #CE, #SE, #SC, #SW").html("");
-  $("#NW, #NC, #NE, #CW, #CC, #CE, #SE, #SC, #SW").css({'background-color':''});
-  $("#ticTacTurn").html("X goes first");
-}
+//######
+//2. Functions (Back End)
+//######
 
+//The player function is called whenever a move is made. It returns either X or O, and updates game1.dude for the next player's turn.
 function player(){
   if (game1.dude === "X"){
     game1.dude = "O";
@@ -28,6 +38,8 @@ function player(){
     return "O";
   }
 }
+
+//The placeReal function is called whenever a move is made. It updates the three arrays which track the game's progress, and checks to see if a win has occurred.
 
 function placeReal(position){
   switch(position){
@@ -85,6 +97,25 @@ function placeReal(position){
       console.log("WHAT DID YOU DO!?");
   }
 }
+
+//The placeVis function is called whenever a move is made. It updates the visual representation of the game state, including displaying win, loss, or draw messages.
+
+function placeVis(position){
+  let move = $("#" + position);
+  const currentDude = player();
+  move.html(currentDude);
+  if (game1.won === true){
+    console.log(currentDude);
+    $("#ticTacTurn").html(currentDude + " has won!");
+    updateScore(currentDude.toLowerCase() + "score");
+  } else if (game1.toprow.length + game1.midrow.length + game1.botrow.length === 9 && !game1.toprow.includes(undefined) && !game1.midrow.includes(undefined) && !game1.botrow.includes(undefined)){
+    $("#ticTacTurn").html("Draw");
+    game1.won = true;
+    $("#playAgain").show();
+  }
+}
+
+//The findHWin, findVWin, & findDWin functions are called whenever a move is made. They check for winning lines Horizontally, Vertically, and Diagonally, and highlight the win if found.
 
 function findHWin(row){
   if (row[0] === row[1] && row[0] === row[2]){
@@ -152,20 +183,30 @@ function findDWin(){
   }
 }
 
-function placeVis(position){
-  let move = $("#" + position);
-  const currentDude = player();
-  move.html(currentDude);
-  if (game1.won === true){
-    console.log(currentDude);
-    $("#ticTacTurn").html(currentDude + " has won!");
-    updateScore(currentDude.toLowerCase() + "score");
-  } else if (game1.toprow.length + game1.midrow.length + game1.botrow.length === 9 && !game1.toprow.includes(undefined) && !game1.midrow.includes(undefined) && !game1.botrow.includes(undefined)){
-    $("#ticTacTurn").html("Draw");
-    game1.won = true;
-    $("#playAgain").show();
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function openSpaceCheck(position){
   switch(position){
@@ -237,93 +278,8 @@ function openSpaceCheck(position){
   }
 }
 
-function maybeplace(position){
-  if (openSpaceCheck(position)){
-    if (game1.won === false){
-      placeReal(position);
-      placeVis(position);
-      console.log("game1.ai is currently set to: " + game1.ai);
-      if (game1.ai === 1){
-        aiMove();
-      } else if (game1.ai === 2){
-        hBlockCheck();
-      }
-    } else {
-      console.log("The game is over. I'm not gonna do shit.");
-    }
-  }    
-}
-
-function vBlockCheck(){
-  const wColumn = [game1.toprow[0], game1.midrow[0], game1.botrow[0]];
-  const cColumn = [game1.toprow[1], game1.midrow[1], game1.botrow[1]];
-  const eColumn = [game1.toprow[2], game1.midrow[2], game1.botrow[2]];
-  const wColumnTest = wColumn.filter(element => element === "X");
-  const cColumnTest = cColumn.filter(element => element === "X");
-  const eColumnTest = eColumn.filter(element => element === "X");
-  console.log("W column is " + wColumn + " wColumnTest is " + wColumnTest);
-  console.log("C column is " + cColumn + " cColumnTest is " + cColumnTest);
-  console.log("E column is " + eColumn + " eColumnTest is " + eColumnTest);
-
-  if (wColumnTest.length === 2 && !wColumn.includes("O")){
-    console.log("Thought you could trick me horizontally?");
-    const place = game1.toprow.findIndex(empty => empty != "X");
-    switch (place){
-      case 0:
-        maybeaiplace("NW");
-        break;
-      case 1:
-        maybeaiplace("CW");
-        break;
-      case -1:
-        maybeaiplace("SW");
-        break;
-      default:
-        console.log("Something broke!");
-        break;
-    }
-  } else if (cColumnTest.length === 2 && !cColumn.includes("O")){
-    console.log("Thought you could trick me horizontally?");
-    const place = game1.midrow.findIndex(empty => empty != "X");
-    switch (place){
-      case 0:
-        maybeaiplace("NC");
-        break;
-      case 1:
-        maybeaiplace("CC");
-        break;
-      case -1:
-        maybeaiplace("SC");
-        break;
-      default:
-        console.log("Something broke!");
-        break;
-    }
-  } else if (eColumnTest.length === 2 && !eColumn.includes("O")) {
-    console.log("Thought you could trick me horizontally?");
-    const place = game1.botrow.findIndex(empty => empty != "X");
-    switch (place){
-      case 0:
-        maybeaiplace("NE");
-        break;
-      case 1:
-        maybeaiplace("CE");
-        break;
-      case -1:
-        maybeaiplace("SE");
-        break;
-      default:
-        console.log("Something broke!");
-        break;
-    }
-  } else {
-    console.log("I got to vBlock, but I didn't find one.");
-    aiMove();
-  }
-}
-
-//SO: This works insofar as it will find and block a line. 
-//BUT: Once it does so, it gets stuck trying to do the same move forever, and the player winds up taking the O turn.
+//Block Checks
+//asdf
 function hBlockCheck(){
   const tRowTest = game1.toprow.filter(element => element === "X");
   const mRowTest = game1.midrow.filter(element => element === "X");
@@ -381,6 +337,127 @@ function hBlockCheck(){
     }
   } else {
     vBlockCheck();
+  }
+}
+
+function vBlockCheck(){
+  const wColumn = [game1.toprow[0], game1.midrow[0], game1.botrow[0]];
+  const cColumn = [game1.toprow[1], game1.midrow[1], game1.botrow[1]];
+  const eColumn = [game1.toprow[2], game1.midrow[2], game1.botrow[2]];
+  const wColumnTest = wColumn.filter(element => element === "X");
+  const cColumnTest = cColumn.filter(element => element === "X");
+  const eColumnTest = eColumn.filter(element => element === "X");
+  console.log("W column is " + wColumn + " wColumnTest is " + wColumnTest);
+  console.log("C column is " + cColumn + " cColumnTest is " + cColumnTest);
+  console.log("E column is " + eColumn + " eColumnTest is " + eColumnTest);
+
+  if (wColumnTest.length === 2 && !wColumn.includes("O")){
+    console.log("Thought you could trick me horizontally?");
+    const place = wColumn.findIndex(empty => empty != "X");
+    console.log("place is" + place);
+    switch (place){
+      case 0:
+        maybeaiplace("NW");
+        break;
+      case 1:
+        maybeaiplace("CW");
+        break;
+      case 2:
+        maybeaiplace("SW");
+        break;
+      default:
+        console.log("Something broke!");
+        break;
+    }
+  } else if (cColumnTest.length === 2 && !cColumn.includes("O")){
+    console.log("Thought you could trick me horizontally?");
+    const place = cColumn.findIndex(empty => empty != "X");
+    console.log("place is" + place);
+    switch (place){
+      case 0:
+        maybeaiplace("NC");
+        break;
+      case 1:
+        maybeaiplace("CC");
+        break;
+      case 2:
+        maybeaiplace("SC");
+        break;
+      default:
+        console.log("Something broke!");
+        break;
+    }
+  } else if (eColumnTest.length === 2 && !eColumn.includes("O")) {
+    console.log("Thought you could trick me horizontally?");
+    const place = eColumn.findIndex(empty => empty != "X");
+    console.log("place is" + place);
+    switch (place){
+      case 0:
+        maybeaiplace("NE");
+        break;
+      case 1:
+        maybeaiplace("CE");
+        break;
+      case 2:
+        maybeaiplace("SE");
+        break;
+      default:
+        console.log("Something broke!");
+        break;
+    }
+  } else {
+    console.log("I got to vBlock, but I didn't find one.");
+    dBlockCheck();
+  }
+}
+
+function dBlockCheck(){
+  const fDiag = [game1.botrow[0], game1.midrow[1], game1.toprow[2]];
+  const bDiag = [game1.toprow[0], game1.midrow[1], game1.botrow[2]];
+  const fDiagTest = fDiag.filter(element => element === "X");
+  const bDiagTest = bDiag.filter(element => element === "X");
+  
+  console.log("forward diagonal is " + fDiag + " fDiagTest is " + fDiagTest);
+  console.log("backward diagonal is " + bDiag + " bDiagTest is " + bDiagTest);
+
+  if (fDiagTest.length === 2 && !fDiag.includes("O")){
+    console.log("I've mastered the diagonal!");
+    const place = fDiag.findIndex(empty => empty != "X");
+    switch (place){
+      case 0:
+        maybeaiplace("SW");
+        break;
+      case 1:
+        maybeaiplace("CC");
+        break;
+      case 2:
+        maybeaiplace("NE");
+        break;
+      default:
+        console.log("Something broke!");
+        break;
+    }
+  } else if (bDiagTest.length === 2 && !bDiag.includes("O")){
+    console.log("I've mastered the diagonal!");
+    const place = bDiag.findIndex(empty => empty != "X");
+    console.log("place is" + place);
+    switch (place){
+      case 0:
+        maybeaiplace("NW");
+        break;
+      case 1:
+        maybeaiplace("CC");
+        break;
+      case 2:
+        maybeaiplace("SE");
+        break;
+      default:
+        console.log("Something broke!");
+        break;
+    }
+  } else {
+    console.log("I got to dBlock, but I didn't find one.");
+    aiMove();
   }
 }
 
@@ -523,50 +600,100 @@ function aiPlayer(level){
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//######
+//ZZZ. Functions (Player Controls)
+//######
+
+//The maybePlace function is called whenever a player clicks a space on the board. It checks if the space is open, calls the functions which make the appropriate move, and summons the AI player if any.
+
+function maybePlace(position){
+  if (openSpaceCheck(position)){
+    if (game1.won === false){
+      placeReal(position);
+      placeVis(position);
+      console.log("game1.ai is currently set to: " + game1.ai);
+      if (game1.ai === 1){
+        aiMove();
+      } else if (game1.ai === 2){
+        hBlockCheck();
+      }
+    } else {
+      console.log("The game is over. I'm not gonna do shit.");
+    }
+  }    
+}
+
+//The newRound function is only available after the game ends and the "Play Again?" button appears.
+
+function newRound(ai) {
+  $("#playAgain").hide();
+  game1 = new Board ([],[],[], false, ai, "X");
+  $("#NW, #NC, #NE, #CW, #CC, #CE, #SE, #SC, #SW").html("");
+  $("#NW, #NC, #NE, #CW, #CC, #CE, #SE, #SC, #SW").css({'background-color':''});
+  $("#ticTacTurn").html("X goes first");
+}
+
+//Attaching attaching listeners to the page's UI.
+
 $(document).ready(function() {
   
   storeScore();
 
-//Attaching listeners to each space on the Tic Tac Toe board.
-
+//Game Board Listeners
   $("#game").on("click", "#NW",function() {
     const position = "NW";
-    maybeplace(position);
+    maybePlace(position);
   }); 
   $("#game").on("click", "#NC",function() {
     const position = "NC";
-    maybeplace(position);
+    maybePlace(position);
   }); 
   $("#game").on("click", "#NE",function() {
     const position = "NE";
-    maybeplace(position);
+    maybePlace(position);
   }); 
   $("#game").on("click", "#CW",function() {
     const position = "CW";
-    maybeplace(position);
+    maybePlace(position);
   }); 
   $("#game").on("click", "#CC",function() {
     const position = "CC";
-    maybeplace(position);
+    maybePlace(position);
   }); 
   $("#game").on("click", "#CE",function() {
     const position = "CE";
-    maybeplace(position);
+    maybePlace(position);
   }); 
   $("#game").on("click", "#SW",function() {
     const position = "SW";
-    maybeplace(position);
+    maybePlace(position);
   }); 
   $("#game").on("click", "#SC",function() {
     const position = "SC";
-    maybeplace(position);
+    maybePlace(position);
   }); 
   $("#game").on("click", "#SE",function() {
     const position = "SE";
-    maybeplace(position);
+    maybePlace(position);
   });
 
-//Attaching listeners to the options menu.
+//Options menu listeners
   $("#options").on("click", "#easyAI",function() {
     gameInProgressCheck(1);
   });
@@ -574,9 +701,8 @@ $(document).ready(function() {
     gameInProgressCheck(2);
   });
 
-//One last listener!
-$("#playAgain").on("click", ".start",function() {
-  newRound(game1.ai);
-});  
-
+//Play again listener
+  $("#playAgain").on("click", ".start",function() {
+    newRound(game1.ai);
+  });  
 });
